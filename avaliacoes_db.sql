da1f688d705c8fcbd1344e2565129b891303da9e
Joao Sergio <joaosergio378@gmail.com>
2025-09-10 13:14:43 -0300

    Add files via upload

---
+++ avaliacoes_db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE avaliacoes_db;
+USE avaliacoes_db;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE avaliacoes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    produto VARCHAR(100) NOT NULL,
+    nota INT NOT NULL,
+    comentario TEXT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
